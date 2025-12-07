648580db5fe0fd3914178dd19d90409bf8c439db
Hayes <liamh@vlsi23.bu.edu>
2025-12-06 20:27:23 -0500

    compile

--- km/meteor_km.mod.c
+++ km/meteor_km.mod.c
@@ -27,10 +27,10 @@ __used
 __attribute__((section("__versions"))) = {
 	{ 0xa16cf51b, "module_layout" },
 	{ 0x97934ecf, "del_timer_sync" },
-	{ 0x1af2db6e, "registered_fb" },
 	{ 0xc6f46339, "init_timer_key" },
 	{ 0xcf8d2e6f, "kmem_cache_alloc_trace" },
 	{ 0xb44e414c, "kmalloc_caches" },
+	{ 0x1af2db6e, "registered_fb" },
 	{ 0x37a0cba, "kfree" },
 	{ 0xd09cfa79, "_mutex_unlock" },
 	{ 0x336aa937, "sys_fillrect" },
