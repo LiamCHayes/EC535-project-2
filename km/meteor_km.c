08290352b5c945ab0fe3818f61eb333b98a01d6e
LiamCHayes <liamcharleshayes@gmail.com>
2025-12-07 02:32:36 -0500

    fix

--- km/meteor_km.c
+++ km/meteor_km.c
@@ -343,7 +343,7 @@ static ssize_t meteor_write(struct file *filp, const char *buf, size_t count, lo
             int x_difference = character_x - meteor_x;
             if (meteor_y > meteor_size + 20) {
                 if (x_difference > 0 && x_difference < meteor_size) {
-                    printk(KERN_ALERT "Collision detected\n", character_x);
+                    printk(KERN_ALERT "Collision detected\n");
                     return -1;
                 }
             }
