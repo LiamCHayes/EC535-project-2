6b422fd25c02503b421a369732ca3090561bb7bc
LiamCHayes <liamcharleshayes@gmail.com>
2025-12-07 01:39:23 -0500

    fix

--- km/meteor_km.c
+++ km/meteor_km.c
@@ -336,7 +336,7 @@ static ssize_t meteor_write(struct file *filp, const char *buf, size_t count, lo
         int x_difference = character_x - meteor_x;
         if (meteor_y > meteor_size + 20) {
             if (x_difference > 0 && x_difference < meteor_size) {
-                return -1;
+                return -2;
             }
         }
     }
